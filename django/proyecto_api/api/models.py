6f89f6c1ce634b1988b9aa70dbbb8535c0ec3e76
Agus2020 <aagustin067@gmail.com>
2023-02-23 23:58:04 -0300

    Arreglado problema de la maquina virtual

--- django/proyecto_api/api/models.py
+++ django/proyecto_api/api/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 
 
+
+class Swi(models.Model):
+    name = models.CharField(max_length=20)
+
 class Computer(models.Model):
     name=models.CharField(max_length=20)
     id_computer=models.CharField(max_length=20)
     id_monitor=models.CharField(max_length=20)
     anydesk=models.CharField(max_length=20)
-    swi=models.PositiveIntegerField()
+    swi=models.ForeignKey(Swi,null=True,on_delete=models.CASCADE)
     puert=models.PositiveIntegerField()
     ip=models.CharField(max_length=20)
     def __str__(self):
