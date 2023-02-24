b80ffa23f35b0ca343e6057a9cfe3af22a25ca38
Agus2020 <aagustin067@gmail.com>
2023-02-24 01:27:46 -0300

    Arreglado problema de tablas

--- django/proyecto_api/api/models.py
+++ django/proyecto_api/api/models.py
@@ -1,17 +1,14 @@
 from django.db import models
 
-
-
-class Swi(models.Model):
-    name = models.CharField(max_length=20)
-
 class Computer(models.Model):
     name=models.CharField(max_length=20)
     id_computer=models.CharField(max_length=20)
     id_monitor=models.CharField(max_length=20)
     anydesk=models.CharField(max_length=20)
-    swi=models.ForeignKey(Swi,null=True,on_delete=models.CASCADE)
+    swi = models.PositiveBigIntegerField()
     puert=models.PositiveIntegerField()
     ip=models.CharField(max_length=20)
     def __str__(self):
         return self.name 
+    
+
