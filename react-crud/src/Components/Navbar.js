bc8c9b6050b09649d62140f034527787073e8b64
Agus2020 <aagustin067@gmail.com>
2023-02-27 01:13:04 -0300

    .

--- react-crud/src/Components/Navbar.js
+++ react-crud/src/Components/Navbar.js
@@ -6,7 +6,10 @@ const Navbar = () => {
     <nav className="navbar navbar-light bg-light">
   <div className="container-fluid">
   <Link to='/' className="navbar-brand">
-  <h3>Informatica DOS</h3>
+    <div className='logo'>
+      <img src="./descarga.png" className='obrasoci' alt="Obrasoci"/>
+      <h3>Informatica DOS</h3>
+    </div>
   </Link>
     <Link to='/computerForm' className="navbar-brand">Upload product</Link>
 
